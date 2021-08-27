4ee6c5d9e4dcd7dcbbd37991e707a7cdb44b2817
tomoaki-kimura <tkim19750404@gmail.com>
2021-08-27 11:16:21 +0900

    add code

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -5,6 +5,9 @@ class PrefecturesController < ApplicationController
   def index
     @prefectures = Prefecture.all
     @prefectures = @prefectures.where(code: params[:code]) if params[:code].present?
+    @prefectures = @prefectures.where(kana: params[:kana]) if params[:kana].present?
+    @prefectures = @prefectures.where(name: params[:name]) if params[:name].present?
+    @prefectures = @prefectures.where(romaji: params[:romaji]) if params[:romaji].present?
     render json: @prefectures
   end
 
