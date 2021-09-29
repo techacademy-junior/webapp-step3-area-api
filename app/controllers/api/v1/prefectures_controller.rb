1ef54ccc7e55ca4a646fc88a50299205ec2cdac4
tomoaki-kimura <tkim19750404@gmail.com>
2021-09-29 09:05:01 +0900

    add area_id

--- app/controllers/api/v1/prefectures_controller.rb
+++ app/controllers/api/v1/prefectures_controller.rb
@@ -5,6 +5,7 @@ class Api::V1::PrefecturesController < ApplicationController
     @prefectures = @prefectures.where(kana: params[:kana]) if params[:kana].present?
     @prefectures = @prefectures.where(name: params[:name]) if params[:name].present?
     @prefectures = @prefectures.where(romaji: params[:romaji]) if params[:romaji].present?
+    @prefectures = @prefectures.where(romaji: params[:area_id]) if params[:area_id].present?
     render json: { status: 'SUCCESS', message: 'Loaded prefectures', data: @prefectures }
   end
 end
